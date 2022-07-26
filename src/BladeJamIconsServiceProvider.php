3949b0bb4babbaaab7d288cbb1defcf3d5573afc
swapnilsarwe <swapnilsarwe@users.noreply.github.com>
2022-07-26 05:14:27 +0000

    Fix styling

--- src/BladeJamIconsServiceProvider.php
+++ src/BladeJamIconsServiceProvider.php
@@ -17,24 +17,24 @@ final class BladeJamIconsServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('blade-jam-icons', []);
 
-            $factory->add('jam-icons', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
+            $factory->add('jam-icons', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/blade-jam-icons.php', 'blade-jam-icons');
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-jam-icons.php', 'blade-jam-icons');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/svg' => public_path('vendor/blade-jam-icons'),
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-jam-icons'),
             ], 'blade-jam-icons');
 
             $this->publishes([
-                __DIR__.'/../config/blade-jam-icons.php' => $this->app->configPath('blade-jam-icons.php'),
+                __DIR__ . '/../config/blade-jam-icons.php' => $this->app->configPath('blade-jam-icons.php'),
             ], 'blade-jam-icons-config');
         }
     }
